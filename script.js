4793f14a510c7c8f1c289939a24c643719ea194d
taeberyy <117457700+taeberyy@users.noreply.github.com>
2026-03-29 11:41:00 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const spawnRate = 200;
 btn.addEventListener("click", func, {once:true});
 
 function func() {
-    alert ("This song about to play reminds me of you!");
+    alert ("This song about to play reminds me of you! I heard it the first time and this was definetely for you.");
     music.play();
 
     function createHP () {
